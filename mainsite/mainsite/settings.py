c174058849dc544588ab02ea8ff2d247fb1ebf3c
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2018-04-19 13:06:27 -0700

    RUNTIME

--- mainsite/mainsite/settings.py
+++ mainsite/mainsite/settings.py
@@ -135,6 +135,7 @@ LOGOUT_REDIRECT_URL = '/login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
