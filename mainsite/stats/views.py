dde24297ac1436db098c9d14779b8bc2b7446450
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2018-04-19 17:03:31 -0700

    Finally

--- mainsite/stats/views.py
+++ mainsite/stats/views.py
@@ -187,7 +187,7 @@ def uni_analysis(request):
     spec_tagu = spec_tag
     spec_tag = spec_tag.lower()
     #print(spec_tag)
-    abc = PDF.objects.filter(pdf_tags__contains=[spec_tag])
+    abc = PDF.objects.filter(pdf_tags__contains=spec_tag)
     total_count = PDF.objects.filter(pdf_tags__contains=[spec_tag]).count()
     uni_list = []
     for one_pdf in list(abc):
